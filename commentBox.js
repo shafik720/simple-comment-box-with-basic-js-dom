b3bc6647b35ab95c22dc24c3d98f383ffd3ba129
shafik720 <shafikrasel5@gmail.com>
2022-07-31 15:57:45 +0600

    xsfgsd

--- commentBox.js
+++ commentBox.js
@@ -1,4 +1,30 @@
 
 document.getElementById('postButton').addEventListener('click',function(){
+    let userName = document.getElementById('nameInput').value;
+
+    // this is name got from user
+    let h5 = document.createElement('h5');
+    h5.innerText = userName;
+
     
+
+    // this is main div for comments div
+    let mainCommentdiv = document.createElement('div');
+    mainCommentdiv.classList.add('comments-box','border','border-2','p-3','rounded','my-3');
+    
+    // adding comments
+    let commentDiv = document.createElement('div');
+    commentDiv.classList.add('comments')
+    
+    // adding names
+    let nameDiv = document.createElement('div');
+    nameDiv.classList.add('name','d-flex','justify-content-end');
+    nameDiv.appendChild(h5);
+    
+    let mainBox = document.getElementById('mainBox');
+    mainBox.appendChild(mainCommentdiv);
+
+    
+    mainCommentdiv.appendChild(nameDiv);
+
 })
